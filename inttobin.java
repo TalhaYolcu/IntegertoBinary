c7082972e816ccd41f3f95fe16569cdef49df7c6
Yakup Talha Yolcu <59283525+TalhaYolcu@users.noreply.github.com>
2021-02-14 11:36:59 +0300

    Java code

---
+++ inttobin.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class inttobin {
+	public static void main(String[] args) {
+	    Scanner inp=new Scanner(System.in);
+	   	int y=inp.nextInt();		
+	    String s=new String();
+	    while(true) {
+			if(y%2==0) {
+				s+="0";
+			}
+			else {
+				if(y==1) {
+					s+="1";
+					break;
+				}
+				else {
+					s+="1";
+				}
+			} 
+			y=y/2;
+	    }
+	    char[] chars = s.toCharArray();
+	    char temp;	
+	    for(int i=0;i<s.length()/2;i++) {
+	    	temp=chars[i];
+	    	chars[i]=chars[s.length()-i-1];
+	    	chars[s.length()-i-1]=temp;
+	    }	
+	    System.out.println(chars);
+	}
+}
