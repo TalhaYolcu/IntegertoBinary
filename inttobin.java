459eccc13731050ae5baa660d7803af6d11888e6
Yakup Talha Yolcu <59283525+TalhaYolcu@users.noreply.github.com>
2021-02-14 11:38:55 +0300

    Java code

--- inttobin.java
+++ inttobin.java
@@ -4,6 +4,7 @@ public class inttobin {
 	    Scanner inp=new Scanner(System.in);
 	   	int y=inp.nextInt();		
 	    String s=new String();
+		//divide until number is 1
 	    while(true) {
 			if(y%2==0) {
 				s+="0";
@@ -19,6 +20,7 @@ public class inttobin {
 			} 
 			y=y/2;
 	    }
+		//we have reverse of the number, we have to reverse it
 	    char[] chars = s.toCharArray();
 	    char temp;	
 	    for(int i=0;i<s.length()/2;i++) {
